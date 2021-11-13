e355921d4c35462d398916d4dccd2bb0a76e1a96
StolzAlexander <alexander.stolz97@gmail.com>
2021-11-13 13:13:41 +0100

    Added folder sass with var.sass and elements.sass

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,19 +7,21 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './modules/auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { CardComponent } from './modules/playground/card/card.component';
-
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, HomeComponent],
+  declarations: [ HomeComponent],
   imports: [
     CommonModule,
     RouterModule
